0c2fb8ebd4eca7a97de3768216e4103bfad87828
DastanLo <dastansulaimankulov98@gmail.com>
2022-07-02 19:48:07 +0600

    KIK

--- client/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export {default as NavBar} from './NavBar';
-export {default as FormElement} from './Form';
-export {default as PostForm} from './PostForm';
-export {default as PostCard} from './PostCard';
-export {default as SubscribeModal} from './Modal';
